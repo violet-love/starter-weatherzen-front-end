643fab0ba4ed39f2be0aa16032069078cdbca048
Violet Love <58837492+violet-love@users.noreply.github.com>
2021-07-22 09:24:19 -0700

    created and exported a Create Observation page with buttons

---
+++ src/observations/ObservationCreate.js
@@ -0,0 +1,39 @@
+import { sumBy } from "lodash";
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+function ObservationCreate() {
+  const history = useHistory();
+
+  function cancelHandler() {
+    history.push("/");
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    history.push("/");
+  }
+
+  return (
+    <main>
+      <h1>Create Observation</h1>
+      <form onSubmit={submitHandler}>
+        <p>Later, input fields will be added here.</p>
+        <div>
+          <button
+            type="button"
+            className="btn btn-secondary mr-2"
+            onClick={cancelHandler}
+          >
+            Cancel
+          </button>
+          <button type="submit" className="btn btn primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </main>
+  );
+}
+
+export default ObservationCreate;
